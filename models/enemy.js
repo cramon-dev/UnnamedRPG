9f49b67f8debdfdf824e3636c4080cc71958951b
reticen <christianr465@gmail.com>
2016-02-01 03:15:22 -0700

    Updated readme.

--- models/enemy.js
+++ models/enemy.js
@@ -1,4 +1,13 @@
-function Enemy() {
+function Enemy(name, str, fin, intl, aff, wil, agl, chr) {
+	this.name = '';
+	this.isDowned = false;
+	this.strength = str;
+	this.finesse = fin;
+	this.intellect = intl;
+	this.affinity = aff;
+	this.will = wil;
+	this.agility = agl;
+	this.charisma = chr;
 }
 
 Enemy.prototype = new Person();
---
+++ readme.md
@@ -0,0 +1,3 @@
+# What is it?
+
+This unnamed RPG is a browser-based game where players must micromanage their partys' status in order fight and survive.
